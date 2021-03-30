7000f1c8acd72976500fa564491490ae16cd6472
saulogp <sauloproetti@gmail.com>
2021-03-30 15:58:31 -0300

    create class

---
+++ prjapiproduto/Models/Product.cs
@@ -0,0 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace prjapiproduto.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Provider Provider { get; set; }
+    }
+}
---
+++ prjapiproduto/Models/Provider.cs
@@ -0,0 +1,14 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace prjapiproduto.Models
+{
+    public class Provider
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
